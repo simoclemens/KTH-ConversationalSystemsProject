f77946a98df94f123a4313235ec455f9b3f09274
simoclemens <117907132+simoclemens@users.noreply.github.com>
2023-12-05 09:49:58 +0100

    corrected prompt

--- chat_bot.py
+++ chat_bot.py
@@ -9,14 +9,12 @@ import os
 os.environ["OPENAI_API_KEY"] = "XXX"
 
 db_path = 'db/test_db'  # sys.argv[1]
-question = "Quali sono le coperture?"  # sys.argv[2]
+question = "Who was the first American president?"  # sys.argv[2]
 
 template = """You are a chatbot having a conversation with a human.
 
 Given the following extracted parts of a long document and a question, create a final answer. When you cannot find 
-information in the context answer "Mi spiace, non ho trovato l'informazione rischiesta". You are allowed to answer 
-only qestions related to insurance policies and yu need to decline any kind of other question aswering "Mi spiace ma 
-non posso rispondere".
+information in the context answer that you don't know.".
 
 {context}
 
@@ -39,13 +37,13 @@ chain = load_qa_chain(llm, chain_type="stuff", memory=memory, prompt=prompt)
 # Load the db from the path
 db = FAISS.load_local(db_path, embeddings)
 
-#docs = db.similarity_search(question, k=10)
-docs = db.max_marginal_relevance_search(question, k=8)
+docs = db.similarity_search(question, k=10)
+# docs = db.max_marginal_relevance_search(question, k=8)
 
 response = chain({"human_input": question,
                   "input_documents": docs,
                   "question": question,
-                  "language": "Italian",
+                  "language": "English",
                   "existing_answer": ""},
                  return_only_outputs=True)
 
